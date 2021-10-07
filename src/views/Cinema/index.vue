d688bbe4e1e3efdf5d2901593e7337afe9255a3b
chineseslz <88058173@qq.com>
2021-10-07 14:57:50 +0800

    一级路由

--- src/views/Cinema/index.vue
+++ src/views/Cinema/index.vue
@@ -1,12 +1,20 @@
 <template>
-    <div>
-        hello cinema
+    <div >
+        <Header title="喵喵影院"/>
+        <TabBar/>
     </div>
 </template>
 
 <script>
+import Header from '../../components/Header'
+import TabBar from '../../components/TabBar'
+
 export default {
-    
+    name : 'Cinema',
+    components:{
+        Header,
+        TabBar
+    }
 }
 </script>
 <style>
--- src/views/Movie/index.vue
+++ src/views/Movie/index.vue
@@ -1,12 +1,19 @@
 <template>
-    <div>
-        hello movie
+    <div >
+        <Header title="喵喵电影"/>
+        <TabBar/>
     </div>
 </template>
 
 <script>
+import Header from '../../components/Header'
+import TabBar from '../../components/TabBar'
 export default {
-    
+    name : 'Movie',
+    components:{
+        Header,
+        TabBar
+    }
 }
 </script>
 <style>
